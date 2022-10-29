157e54837d7a30f9e8ff565907fcc21a67d30a23
Ruggero-Angalone <ruggeroangalone@gmail.com>
2022-10-29 23:24:10 +0200

    added txt support

--- main.py
+++ main.py
@@ -18,11 +18,10 @@ def extensionFromPdfToTxt(filenamePdf):
     filenameToTxt = filenamePdf
     filenameToTxt = filenameToTxt.replace(".pdf",".txt")
     return filenameToTxt
-def createAndSaveTxtFileFromOpenedPdf(reader, filenameToTxt):
+def createAndSaveTxtFileFromOpenedPdf(text, filenameToTxt):
     fileTxt = open(filenameToTxt, "w")
-    for page in reader.pages:
-        fileTxt.write(page.extract_text())
-        fileTxt.write("\n")
+    fileTxt.write(text)
+    fileTxt.write("\n")
     fileTxt.close()
 
 def isFileTypePdf(filename):
@@ -34,22 +33,18 @@ def appendCsvFindToFilename(filename):
     filenameCsvWithFind = filenameCsvWithFind + "_CsvFind.txt"
     return filenameCsvWithFind
 
-def writeCsvWithRegexMatch_RegexInLine(fileCsvWithFind, reader):
-    for page in reader.pages:
-        text = page.extract_text()
-        bufText = io.StringIO(text)
-        for line in bufText.readlines():
-            regexMatches = re.finditer(regexPatternToFind,line)
-            for regexMatch in regexMatches:
-                if(regexMatch):
-                    foundRegexGroup = regexMatch.group(regexGroupNumber)
-                    if(foundRegexGroup):
-                        stringToWrite = foundRegexGroup + " \t " + line
-                        fileCsvWithFind.write(stringToWrite)
-def writeCsvWithRegexMatch_RegexNotInLine(fileCsvWithFind, reader):
-    text = ""
-    for page in reader.pages:
-        text = text + page.extract_text()
+def writeCsvWithRegexMatch_RegexInLine(fileCsvWithFind, text):
+    bufText = io.StringIO(text)
+    for line in bufText.readlines():
+        regexMatches = re.finditer(regexPatternToFind,line)
+        for regexMatch in regexMatches:
+            if(regexMatch):
+                foundRegexGroup = regexMatch.group(regexGroupNumber)
+                if(foundRegexGroup):
+                    stringToWrite = foundRegexGroup + " \t " + line
+                    fileCsvWithFind.write(stringToWrite)
+
+def writeCsvWithRegexMatch_RegexNotInLine(fileCsvWithFind, text):
     regexMatches = re.finditer(regexPatternToFind,text)
     for regexMatch in regexMatches:
         if(regexMatch):
@@ -57,29 +52,46 @@ def writeCsvWithRegexMatch_RegexNotInLine(fileCsvWithFind, reader):
             if(foundRegexGroup):
                 fileCsvWithFind.write(foundRegexGroup + "\n")
 
+def extractTextFromPdf(reader):
+    text = ""
+    for page in reader.pages:
+        text = text + page.extract_text()
+    return text
+
 def main():
     filename = sys.argv[1]
     if( not(isFileTypePdf(filename) or isFileTypeTxt(filename)) ):
         logging.error("tried to open a non supported file: ",filename)
         sys.exit("Error: This script only accepts file ending with .pdf or .txt")
-    try:
-        reader = PyPDF2.PdfReader(filename)
-        logging.info("opened file: "+ filename)
-    except Exception:
-        logging.error(sys.exc_info()[1])
-        sys.exit(print(sys.exc_info()[1]))
     if(isFileTypePdf(filename)):
-        logging.info("file " + filename + " is pdf and is going to be converted to txt")
-        createAndSaveTxtFileFromOpenedPdf(reader, extensionFromPdfToTxt(filename))
-        logging.info("file " + filename + " converted into " + extensionFromPdfToTxt(filename))
+        try:
+            reader = PyPDF2.PdfReader(filename)
+            logging.info("opened file: "+ filename)
+            text = extractTextFromPdf(reader)
+            logging.info("file " + filename + " is pdf and is going to be converted to txt")
+            createAndSaveTxtFileFromOpenedPdf(text, extensionFromPdfToTxt(filename))
+            logging.info("file " + filename + " converted into " + extensionFromPdfToTxt(filename))
+        except Exception:
+            logging.error(sys.exc_info()[1])
+            sys.exit(print(sys.exc_info()[1]))
+    elif(isFileTypeTxt(filename)):
+        try:
+            txtFile = open(filename, "r")
+            text=txtFile.read()
+            txtFile.close()
+            logging.info("opened file: "+ filename)
+        except Exception:
+            logging.error(sys.exc_info()[1])
+            sys.exit(print(sys.exc_info()[1]))
+
     fileCsvWithFind = open(appendCsvFindToFilename(filename), "w")
     logging.info("opened file where the regex match will be written with name: "+appendCsvFindToFilename(filename))
     if(regexInLine):
         fileCsvWithFind.write("MATCH \t CONTEXT \n")
-        writeCsvWithRegexMatch_RegexInLine(fileCsvWithFind, reader)
+        writeCsvWithRegexMatch_RegexInLine(fileCsvWithFind, text)
     else:
         fileCsvWithFind.write("MATCH \n")
-        writeCsvWithRegexMatch_RegexNotInLine(fileCsvWithFind, reader)
+        writeCsvWithRegexMatch_RegexNotInLine(fileCsvWithFind, text)
     fileCsvWithFind.close()
 
 
